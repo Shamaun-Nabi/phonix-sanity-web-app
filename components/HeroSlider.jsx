2ec21e6ac5ce963e8158074e32eff1288f9d7830
Shamaun-Nabi <snashik950@gmail.com>
2022-12-16 19:14:07 +0600

    ignore browser warning for token & remove unwanted sanity folder

--- components/HeroSlider.jsx
+++ components/HeroSlider.jsx
@@ -3,16 +3,20 @@ import React from "react";
 import { urlFor } from "../lib/client";
 
 const HeroSlider = ({ bannerInfo }) => {
-  console.log(bannerInfo);
+  // console.log(bannerInfo);
   return (
     <div className="hero-banner-container">
       <div>
         <p className="beats-solo">{bannerInfo?.smallText}</p>
         <h3>{bannerInfo.midText}</h3>
         <h1>{bannerInfo.largeText1}</h1>
-        <img src={urlFor(bannerInfo.image)} alt="headphones" className="hero-banner-image" />
+        <img
+          src={urlFor(bannerInfo.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
         <div>
-          <Link href="product/ID">
+          <Link href={`{product/${bannerInfo.product}`}>
             <button type="button">{bannerInfo.buttonText}</button>
           </Link>
         </div>
